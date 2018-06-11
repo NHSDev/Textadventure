2e7415865ba405c6cfbcde56fe49f52c389921b2
NHSDev <Nick_Struik@outlook.com>
2018-06-11 14:22:28 +0200

    first commit

---
+++ Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ZuulCS
+{
+    public class Player
+    {
+        private Room _currentRoom;
+        private int health;
+        private uint damage;
+
+        public Room currentRoom {
+            get { return _currentRoom; }
+            set { _currentRoom = value; }
+        }
+
+        private void Damage() { 
+
+        }
+    }
+}
